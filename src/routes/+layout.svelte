c635d5a86853638ff6dec506016dbf5713f51e36
Christian van Ommeren <git@christian.surf>
2022-12-15 14:24:23 +0100

    Center align on widescreen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,15 @@
   import '../app.css'
 </script>
 
-<div class="m-20 bg-white rounded-xl">
-  <slot />
+<div class="flex justify-center">
+  <div class="m-20 max-w-fit bg-white rounded-xl">
+    <slot />
+  </div>
+</div>
+
+<div class="text-center">
+  <a class="text-sm text-gray-700" href="https://squid.software" target="_blank" rel="noreferrer">
+    🦑
+    <span class="underline underline-offset-4">Made by Squid</span>
+  </a>
 </div>
